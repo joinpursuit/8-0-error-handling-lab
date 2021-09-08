373155089a1dabb4262f05a2464325bb67416803
salrey <tekdehn@gmail.com>
2021-09-08 12:45:12 -0400

    Update function to try and catch all errors

--- index.js
+++ index.js
@@ -78,11 +78,17 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+    let total = 0
+    try {
+      const filteredProducts = filterProductsByPriceRange(products, min, max);
+      const total = getCartTotal(filteredProducts);
+      return total 
+    } catch (error) {
+      return total
+    }
 }
+
+
 
 module.exports = {
   getCartTotal,
