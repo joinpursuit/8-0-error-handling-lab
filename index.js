a0fc3f3be2d471ce225c80ab07d62fc6508c811a
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-08 12:36:55 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/*
-  Do not change the line below. If you'd like to run code from this file, you may use the `exampleSongData` variable below to gain access to tickets data. This data is pulled from the `data/songs.js` file.
-
-  You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
-
-  Keep in mind that your functions must still have and use a parameter for accepting all songs.
-*/
 const exampleProducts = [
   { id: 1, name: "Panel Headboard", priceInCents: 12332 },
   { id: 2, name: "Low Profile Sleigh Bed", priceInCents: 22999 },
@@ -12,32 +5,39 @@ const exampleProducts = [
   { id: 4, name: "Abstract Light Gray Area Rug", priceInCents: 33999 },
   { id: 5, name: "Multi Game Table", priceInCents: 81743 },
 ];
-// Do not change the line above.
 
-/*
-  This function should throw an error if:
-  - The `cart` array is empty.
-*/
 function getCartTotal(cart) {
   let result = 0;
+  if (!cart || cart.length == 0) {
+    throw "Error: The cart is empty!";
+  }
   for (let product of cart) {
     result += product.priceInCents;
   }
   return result;
 }
 
-/*
-  This function should throw an error if:
-  - The `products` array is empty.
-  - Either `min` or `max` is not a number.
-  - `max` is equal to `0`.
-  - `min` is greater than `max`.
-  - Either `min` or `max` is less than `0`.
-  - Any of the products in the `products` array does not have a `priceInCents` key.
-*/
 function filterProductsByPriceRange(products, min, max) {
   const result = [];
+  if (products.length == 0) {
+    throw "Error: Products Array not found!";
+  }
+  if (typeof min != "number" || typeof max != "number") {
+    throw "Error: Min/Max is not a number!";
+  }
+  if (min > max) {
+    throw "Error: Min is greater than Max!";
+  }
+  if (max == 0) {
+    throw "Error: Max is equal to zero!";
+  }
+  if (max < 0 || min < 0) {
+    throw "Error: Min/Max is less than zero!";
+  }
   for (let product of products) {
+    if (!product.priceInCents) {
+      throw "Error: Price in cents not defined!";
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -45,14 +45,14 @@ function filterProductsByPriceRange(products, min, max) {
   return result;
 }
 
-/*
-  If any errors occur in this function, it should return `0`.
-*/
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch (err) {
+    return 0;
+  }
 }
 
 module.exports = {
