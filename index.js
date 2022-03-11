ba062bf8faff92cd6be3c68f334df73c8e8f2068
meera-ramesh19 <meeraramesh19@outlook.com>
2022-03-11 15:15:54 -0500

    error-handling-lab

--- index.js
+++ index.js
@@ -6,11 +6,11 @@
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
 const exampleProducts = [
-  { id: 1, name: "Panel Headboard", priceInCents: 12332 },
-  { id: 2, name: "Low Profile Sleigh Bed", priceInCents: 22999 },
-  { id: 3, name: "Oval 100% Cotton Solid Bath Rug", priceInCents: 1399 },
-  { id: 4, name: "Abstract Light Gray Area Rug", priceInCents: 33999 },
-  { id: 5, name: "Multi Game Table", priceInCents: 81743 },
+  { id: 1, name: 'Panel Headboard', priceInCents: 12332 },
+  { id: 2, name: 'Low Profile Sleigh Bed', priceInCents: 22999 },
+  { id: 3, name: 'Oval 100% Cotton Solid Bath Rug', priceInCents: 1399 },
+  { id: 4, name: 'Abstract Light Gray Area Rug', priceInCents: 33999 },
+  { id: 5, name: 'Multi Game Table', priceInCents: 81743 },
 ];
 // Do not change the line above.
 
@@ -20,6 +20,7 @@ const exampleProducts = [
 */
 function getCartTotal(cart) {
   let result = 0;
+  if (cart.length === 0) throw 'Error!! The array is empty';
   for (let product of cart) {
     result += product.priceInCents;
   }
@@ -30,29 +31,45 @@ function getCartTotal(cart) {
   This function should throw an error if:
   - The `products` array is empty.
   - Either `min` or `max` is not a number.
-  - `max` is equal to `0`.
-  - `min` is greater than `max`.
-  - Either `min` or `max` is less than `0`.
-  - Any of the products in the `products` array does not have a `priceInCents` key.
-*/
+//   - `max` is equal to `0`.
+//   - `min` is greater than `max`.
+//   - Either `min` or `max` is less than `0`.
+//   - Any of the products in the `products` array does not have a `priceInCents` key.
+// */
+
 function filterProductsByPriceRange(products, min, max) {
+  if (!products.length) {
+    throw 'Error!!,Products array should not be empty';
+  }
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw 'Error!!, max or min value should be a number';
+  }
+  if (min > max || min < 0 || max < 0) {
+    throw 'Error!!, max or min value  is incorrect';
+  } else if (max === 0) {
+    throw 'Error!! max value cannot be zero';
+  }
   const result = [];
   for (let product of products) {
+    if (!product.priceInCents) {
+      throw 'Error!! Price not found';
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
   }
   return result;
 }
-
 /*
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch (error) {}
+  return 0;
 }
 
 module.exports = {
