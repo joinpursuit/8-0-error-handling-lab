c4e1e3024d0d8011c21aeb2b925957d1054458d9
named.josie <named.josie@gmail.com>
2022-03-11 17:57:44 -0500

    Completed Lab

--- index.js
+++ index.js
@@ -6,11 +6,11 @@
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
 const exampleProducts = [
-  { id: 1, name: "Panel Headboard", priceInCents: 12332 },
-  { id: 2, name: "Low Profile Sleigh Bed", priceInCents: 22999 },
-  { id: 3, name: "Oval 100% Cotton Solid Bath Rug", priceInCents: 1399 },
-  { id: 4, name: "Abstract Light Gray Area Rug", priceInCents: 33999 },
-  { id: 5, name: "Multi Game Table", priceInCents: 81743 },
+  { id: 1, name: 'Panel Headboard', priceInCents: 12332 },
+  { id: 2, name: 'Low Profile Sleigh Bed', priceInCents: 22999 },
+  { id: 3, name: 'Oval 100% Cotton Solid Bath Rug', priceInCents: 1399 },
+  { id: 4, name: 'Abstract Light Gray Area Rug', priceInCents: 33999 },
+  { id: 5, name: 'Multi Game Table', priceInCents: 81743 },
 ];
 // Do not change the line above.
 
@@ -22,6 +22,9 @@ function getCartTotal(cart) {
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
+  }
+  if (!cart.length) {
+    throw `The cart array is empty`;
   }
   return result;
 }
@@ -37,9 +40,28 @@ function getCartTotal(cart) {
 */
 function filterProductsByPriceRange(products, min, max) {
   const result = [];
+
+  if (products.length === 0) {
+    throw `The ${products} array is empty`;
+  }
+  if (typeof min !== 'number' || typeof max !== 'number') {
+    throw `Either ${min} or ${max} is not a number`;
+  }
+  if (max === 0) {
+    throw `${max} is equal to 0`;
+  }
+  if (min > max) {
+    throw `${min} is greater than ${max}.`;
+  }
+  if (min < 0 || max < 0) {
+    throw `Either ${min} or ${max} is less than 0`;
+  }
   for (let product of products) {
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
+    }
+    if (!product.priceInCents) {
+      throw `Any of the products in the ${products} array does not have a ${priceInCents} key`;
     }
   }
   return result;
@@ -49,10 +71,13 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch {
+    return 0;
+  }
 }
 
 module.exports = {
