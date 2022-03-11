6d8c769d4ce51c9d210e71f6c3f9d3e3d8297c74
Derek <plutus.derekpeterson@gmail.com>
2022-03-11 15:20:51 -0500

    Completed Error Handling lab

--- index.js
+++ index.js
@@ -6,11 +6,11 @@
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
 const exampleProducts = [
-  { id: 1, name: "Panel Headboard", priceInCents: 12332 },
-  { id: 2, name: "Low Profile Sleigh Bed", priceInCents: 22999 },
-  { id: 3, name: "Oval 100% Cotton Solid Bath Rug", priceInCents: 1399 },
-  { id: 4, name: "Abstract Light Gray Area Rug", priceInCents: 33999 },
-  { id: 5, name: "Multi Game Table", priceInCents: 81743 },
+  { id: 1, name: 'Panel Headboard', priceInCents: 12332 },
+  { id: 2, name: 'Low Profile Sleigh Bed', priceInCents: 22999 },
+  { id: 3, name: 'Oval 100% Cotton Solid Bath Rug', priceInCents: 1399 },
+  { id: 4, name: 'Abstract Light Gray Area Rug', priceInCents: 33999 },
+  { id: 5, name: 'Multi Game Table', priceInCents: 81743 },
 ];
 // Do not change the line above.
 
@@ -18,13 +18,23 @@ const exampleProducts = [
   This function should throw an error if:
   - The `cart` array is empty.
 */
-function getCartTotal(cart) {
-  let result = 0;
-  for (let product of cart) {
-    result += product.priceInCents;
-  }
-  return result;
-}
+//Traditional way to solve this function using the for...of loop:
+// function getCartTotal(cart) {
+//   let result = 0;
+//   if (cart.length === 0) throw 'Error!';
+//   for (let product of cart) {
+//     result += product.priceInCents;
+//   }
+//   return result;
+// }
+
+//Use native array method (.reduce) and arrow function to solve this problem:
+const getCartTotal = (cart) => {
+  if (!cart.length) throw 'Error!';
+  return cart.reduce((acc, curr) => {
+    return acc + curr.priceInCents;
+  }, 0);
+};
 
 /*
   This function should throw an error if:
@@ -35,25 +45,70 @@ function getCartTotal(cart) {
   - Either `min` or `max` is less than `0`.
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
-function filterProductsByPriceRange(products, min, max) {
-  const result = [];
-  for (let product of products) {
-    if (product.priceInCents >= min && product.priceInCents <= max) {
-      result.push(product);
-    }
+//Traditional way to solve this by using the for...of loop:
+// function filterProductsByPriceRange(products, min, max) {
+//   if (
+//     !products.length ||
+//     max === 0 ||
+//     min > max ||
+//     max < 0 ||
+//     min < 0 ||
+//     typeof max !== 'number' ||
+//     typeof min !== 'number'
+//   ) {
+//     throw 'Error!';
+//   }
+//   const result = [];
+//   for (let product of products) {
+//     if (!product.priceInCents) {
+//       throw 'Error!';
+//     }
+//     if (min <= product.priceInCents && product.priceInCents <= max) {
+//       result.push(product);
+//     }
+//   }
+//   return result;
+// }
+
+//Using the native array method (.filter) and arrow function to solve this:
+const filterProductsByPriceRange = (products, min, max) => {
+  if (
+    !products.length ||
+    max === 0 ||
+    min > max ||
+    max < 0 ||
+    min < 0 ||
+    typeof max !== 'number' ||
+    typeof min !== 'number'
+  ) {
+    throw 'Error!';
   }
+  let result = products.filter((product) => {
+    if (!product.priceInCents) {
+      throw 'Error!';
+    }
+    return min <= product.priceInCents && product.priceInCents <= max;
+  });
   return result;
-}
+};
+
+//console.log(filterProductsByPriceRange(exampleProducts, 10000, 30000))
 
 /*
   If any errors occur in this function, it should return `0`.
 */
-function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
 
-  return total;
+function getTotalOfAllProductsByPriceRange(products, min, max) {
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch (error) {
+    return 0;
+  }
 }
+
+//Do not change any code below this line:
 
 module.exports = {
   getCartTotal,
