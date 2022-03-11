b1b1290136dc189dd3c6edc2f7c54dd8b7c58470
Jonathan Scheiber <jon@scheiber.me>
2022-03-11 14:56:51 -0500

    complete getTotalOfAllProductsByPriceRange function

--- index.js
+++ index.js
@@ -74,10 +74,12 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
-
-  return total;
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
+    return total;
+  } catch (error) {}
+  return 0;
 }
 
 module.exports = {
