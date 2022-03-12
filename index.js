50794aba64cce1f3fc73dc198f7df96aff5bdbf5
Tom LaTulipe <tlatulipe@gmail.com>
2022-03-12 17:47:21 -0500

    error checking on #2 working - main not?

--- index.js
+++ index.js
@@ -5,12 +5,31 @@
 
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
 */
-const exampleProducts = [
-  { id: 1, name: "Panel Headboard", priceInCents: 12332 },
-  { id: 2, name: "Low Profile Sleigh Bed", priceInCents: 22999 },
-  { id: 3, name: "Oval 100% Cotton Solid Bath Rug", priceInCents: 1399 },
-  { id: 4, name: "Abstract Light Gray Area Rug", priceInCents: 33999 },
-  { id: 5, name: "Multi Game Table", priceInCents: 81743 },
+const exampleProducts = [{
+    id: 1,
+    name: "Panel Headboard",
+    priceInCents: 12332
+  },
+  {
+    id: 2,
+    name: "Low Profile Sleigh Bed",
+    priceInCents: 22999
+  },
+  {
+    id: 3,
+    name: "Oval 100% Cotton Solid Bath Rug",
+    priceInCents: 1399
+  },
+  {
+    id: 4,
+    name: "Abstract Light Gray Area Rug",
+    priceInCents: 33999
+  },
+  {
+    id: 5,
+    name: "Multi Game Table",
+    priceInCents: 81743
+  },
 ];
 // Do not change the line above.
 
@@ -23,19 +42,24 @@ function getCartTotal(cart) {
   for (let product of cart) {
     result += product.priceInCents;
   }
+  if (result == 0) {
+    throw error; // <--- Remember to throw the error within the function
+  }
   return result;
-};
+}
 
 try {
-  getCartTotal(cart);
-} catch (error){
+  getCartTotal(cart)
+} catch (error) {
   console.error(error);
 }
 
+
+
 /*
   This function should throw an error if:
-  - The `products` array is empty.
-  - Either `min` or `max` is not a number.
+  - The `products` array is empty. v
+  - Either `min` or `max` is not a number. v
   - `max` is equal to `0`.
   - `min` is greater than `max`.
   - Either `min` or `max` is less than `0`.
@@ -43,12 +67,31 @@ try {
 */
 function filterProductsByPriceRange(products, min, max) {
   const result = [];
+  if (products == []) {
+    throw error;
+  }
+  if (!min.isInteger() || !max.isInteger()) {
+    throw error;
+  }
+  if (max <= 0 || min < 0 || min > max) {
+    throw error;
+  }
+  if (!([priceInCents] in products)) {
+    throw error;
+  }
+
   for (let product of products) {
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
   }
   return result;
+}
+
+try {
+  filterProductsByPriceRange(products, min, max);
+} catch (error) {
+  console.error(error);
 }
 
 /*
