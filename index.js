c4076e62513c914563bbe1a4369cabbebe7f217a
fakanbiomowunmi@gmail.com <fakanbiomowunmi@gmail.com>
2022-03-12 23:22:52 -0500

    Error

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+  if (cart.length < 1){
+  throw "Error"
+  }
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -36,8 +39,21 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
+  if(products.length < 1){
+    throw "Error"
+  }
+  if(typeof min !== 'number' || typeof max !== 'number'){
+    throw "This is not a number!"
+  }
+  if(min <= 0 || max <= 0 ){
+    throw "This number is not greater than 0!"
+  }
+  
   const result = [];
   for (let product of products) {
+    if (!product.priceInCents) {
+      throw "Error"
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,10 +65,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
+  try {
   const filteredProducts = filterProductsByPriceRange(products, min, max);
   const total = getCartTotal(filteredProducts);
 
   return total;
+  }catch (error) {
+    return 0
+  }
 }
 
 module.exports = {
