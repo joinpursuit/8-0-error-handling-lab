bc93cf0a8b7192e17bb704bebf87c80b7debc76f
King Ankoue <kingankoue@Kings-MacBook-Air.local>
2022-04-05 20:35:13 -0400

    errorHandling

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+  if(cart.length < 1){
+    throw"this cart is empty"
+  }
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -36,14 +39,28 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
+  if(products.length < 1){
+    throw "No products"
+  }
+  if(typeof min !== 'number' || typeof max !== 'number'){
+    throw "This is not a number!"
+  }
+  if(min <= 0 || max <= 0 ){
+    throw "This number is not greater than 0!"
+  }
+  
   const result = [];
   for (let product of products) {
+    if(!product.priceInCents){
+      throw "Error!"
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
   }
   return result;
 }
+
 
 /*
   If any errors occur in this function, it should return `0`.
