94dc86e14cfe61e589cf4a2701940d4052656874
MarsiaRochelin <marsrochelin@pursuit.org>
2022-08-01 14:41:59 -0400

    last func

--- index.js
+++ index.js
@@ -67,14 +67,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-// try {
-//   const filteredProducts = filterProductsByPriceRange(products, min, max);
-//   const total = getCartTotal(filteredProducts);
+try {
+  const filteredProducts = filterProductsByPriceRange(products, min, max);
+  const total = getCartTotal(filteredProducts);
 
-//   return total; 
-// } catch (error) {
-//   return 0
-// }
+  return total; 
+} catch (error) {
+  return 0
+}
 
   }
   
