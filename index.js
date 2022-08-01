f6c5679cdee6d3884d01c7c8f7a10a54aba41c2b
Daniel-Mazzilli <danmazzilli@pursuit.org>
2022-08-01 07:05:46 -0400

    first two functions

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const exampleProducts = [
 */
 function getCartTotal(cart) {
   let result = 0;
+  if (cart.length < 1) {
+    throw `cart is empty.`
+  }
   for (let product of cart) {
     result += product.priceInCents;
   }
@@ -36,8 +39,26 @@ function getCartTotal(cart) {
   - Any of the products in the `products` array does not have a `priceInCents` key.
 */
 function filterProductsByPriceRange(products, min, max) {
+  if(products.length < 1) {
+    throw `There must be at least one product.`
+  }
+  if(typeof min !== `number` || typeof max !== `number`) {
+    throw `min and max must be numeric values.`
+  }
+  if (max === 0) {
+    throw `max cannot be equal to 0.`
+  }
+  if (min > max) {
+    throw `min needs to be a smaller value than max.`
+  }
+  if (min < 0 || max < 0) {
+    throw `min and max must be larger than 0.`
+  }
   const result = [];
   for (let product of products) {
+    if (product.priceInCents === undefined) {
+      throw `All products must have a price in cents value.`
+    }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
