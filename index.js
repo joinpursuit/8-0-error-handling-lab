5908b5fcc7a58afcdb211eb1811571c006da2e5f
ayd823 <aydiaz823@gmail.com>
2022-08-06 20:02:12 -0400

    Error Handling Lab

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+ if (cart.length === 0) {
+  throw "Error";
+ }
+
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -37,7 +41,26 @@ function getCartTotal(cart) {
 */
 function filterProductsByPriceRange(products, min, max) {
   const result = [];
+if (products.length === 0) {
+  throw "Error";
+}
+
   for (let product of products) {
+    if (!product.priceInCents) {
+      throw "Error";
+    }
+    if (typeof min !== "number" || typeof max !== "number") {
+      throw "Error";
+    }
+
+    if (min < 0 || max <= 0) {
+      throw "Error"
+    }
+
+    if (min > max) {
+      throw "Error"
+    }
+
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
@@ -49,10 +72,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
+  try {
   const filteredProducts = filterProductsByPriceRange(products, min, max);
   const total = getCartTotal(filteredProducts);
 
-  return total;
+  return total; }
+  catch(error) {
+    return 0;
+  }
 }
 
 module.exports = {
