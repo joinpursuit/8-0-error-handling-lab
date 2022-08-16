9a7ce9bdf9ea2b4b9b697367d6c2e43753aae46e
SasukeDaTony <tonytaylor@pursuit.org>
2022-08-15 21:32:14 -0400

    index-complete

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const exampleProducts = [
   - The `cart` array is empty.
 */
 function getCartTotal(cart) {
+if (cart.length === 0) {
+  throw "The `cart` array is empty"
+}
   let result = 0;
   for (let product of cart) {
     result += product.priceInCents;
@@ -38,9 +41,24 @@ function getCartTotal(cart) {
 function filterProductsByPriceRange(products, min, max) {
   const result = [];
   for (let product of products) {
+    if (!product.priceInCents)  {
+      throw "does not have PriceInCents key"
+     }
     if (product.priceInCents >= min && product.priceInCents <= max) {
       result.push(product);
     }
+  }
+  if (products.length === 0 ) {
+    throw "The `products` array is empty."
+  }
+  if (max === 0) {
+    throw "`max` is less than or equal to zero"
+  }
+  if (min < 0 || max < 0) {
+    throw "`min` is greater than `max` or less than zero.";
+  }
+  if (typeof(min) !== "number" || typeof(max) !== "number") {
+    throw "`min` or `max` is not a number."
   }
   return result;
 }
@@ -49,10 +67,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
+  try {
   const filteredProducts = filterProductsByPriceRange(products, min, max);
   const total = getCartTotal(filteredProducts);
-
   return total;
+  }
+  catch (error) {
+    return 0
+  }
 }
 
 module.exports = {
