8e3daa218bb2ac88fa4abe38b44c8e1bc41e8ee1
salvagebar <salvagebar@gmail.com>
2022-11-21 19:22:01 -0500

    getTotalOfAllProductsByPriceRange() passed all tests -- All tests passed

--- index.js
+++ index.js
@@ -74,10 +74,14 @@ function filterProductsByPriceRange(products, min, max) {
   If any errors occur in this function, it should return `0`.
 */
 function getTotalOfAllProductsByPriceRange(products, min, max) {
-  const filteredProducts = filterProductsByPriceRange(products, min, max);
-  const total = getCartTotal(filteredProducts);
+  try {
+    const filteredProducts = filterProductsByPriceRange(products, min, max);
+    const total = getCartTotal(filteredProducts);
 
-  return total;
+    return total;
+  } catch (error) {
+    return 0;
+  }
 }
 
 module.exports = {
